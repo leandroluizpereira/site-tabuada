dfc4b27d7d5e9d76804998f2fa7298021567bde7
leandroluizpereira <leandro.2306@hotmail.com>
2021-06-27 12:50:57 -0300

    Update funcao.js

--- static/funcao.js
+++ static/funcao.js
@@ -5,7 +5,7 @@ function tabuada(){
 
     document.write("<h1> </h1>");
 
-    for(var i =0;i<1002;i++){
+    for(var i =0;i<1012;i++){
         document.write("<h2> A tabuada do "+i+" </h2>");
         for(var k =0;k<11;k++){
             var mult = i * k;
