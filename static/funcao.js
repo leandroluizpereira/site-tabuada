b5a3f58d1b607874dba63dddcaecdb6b3baea8fe
leandroluizpereira <leandro.2306@hotmail.com>
2021-06-27 12:41:46 -0300

    javascript

--- static/funcao.js
+++ static/funcao.js
@@ -7,7 +7,7 @@ function tabuada(){
 
     for(var i =0;i<1002;i++){
         document.write("<h2> A tabuada do "+i+" </h2>");
-        for(var k =0;k<1002;k++){
+        for(var k =0;k<11;k++){
             var mult = i * k;
 
      document.write("<h1> "+i+" x "+k+" = "+mult+" </h1>");
