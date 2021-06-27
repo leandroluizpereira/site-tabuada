97921d186a17fd937c31f5b857c2f3ca19b3401f
leandroluizpereira <leandro.2306@hotmail.com>
2021-06-27 12:00:33 -0300

    style

---
+++ static/funcao.js
@@ -0,0 +1,21 @@
+
+
+
+function tabuada(){
+
+    document.write("<h1> </h1>");
+
+    for(var i =0;i<11;i++){
+        document.write("<h2> A tabuada do "+i+" </h2>");
+        for(var k =0;k<11;k++){
+            var mult = i * k;
+
+     document.write("<h1> "+i+" x "+k+" = "+mult+" </h1>");
+ 
+        }
+  
+    }
+
+    document.write("<h1> </h1>");
+}
+
